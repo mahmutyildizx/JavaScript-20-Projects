84508c0b86da46d638d9a44dc16d9495e73a1e6e
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-03-19 23:11:37 +0300

    animated navigation functions added

---
+++ animated-navigation/script.js
@@ -0,0 +1,34 @@
+const menuBars = document.querySelector("#menu-bars");
+const overlay = document.querySelector("#overlay");
+const nav1 = document.querySelector("#nav-1");
+const nav2 = document.querySelector("#nav-2");
+const nav3 = document.querySelector("#nav-3");
+const nav4 = document.querySelector("#nav-4");
+const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+const navAnimation = (direction1, direction2) => {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+};
+
+const toggleNav = () => {
+  menuBars.classList.toggle("change");
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    navAnimation("out", "in");
+  } else {
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    navAnimation("in", "out");
+  }
+};
+
+menuBars.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
