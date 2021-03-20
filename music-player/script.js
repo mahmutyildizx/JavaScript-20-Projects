96d26b2779f411a437ee8b12b36dd33fddbb38e0
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-03-20 22:51:07 +0300

    music player functions added and source names changed

---
+++ music-player/script.js
@@ -0,0 +1,116 @@
+const image = document.querySelector("img");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
+const music = document.querySelector("audio");
+const currentTimeEl = document.querySelector("#current-time");
+const durationEl = document.querySelector("#duration");
+const progress = document.querySelector("#progress");
+const progressContainer = document.querySelector("#progress-container");
+const prevBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const nextBtn = document.querySelector("#next");
+
+const songs = [
+  {
+    name: "source-1",
+    displayName: "Electric Chill Machine",
+    artist: "Lorem Ipsum",
+  },
+  {
+    name: "source-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Lorem Ipsum",
+  },
+  {
+    name: "source-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Lorem Ipsum",
+  },
+  {
+    name: "source-4",
+    displayName: "Front Row (Remix)",
+    artist: "Lorem Ipsum",
+  },
+];
+
+let isPlaying = false;
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
